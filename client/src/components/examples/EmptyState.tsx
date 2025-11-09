d3160213cd24332d7d3d54c44737985a7437250e
NOTAHACKER9999 <th15isar0bothi@gmail.com>
2025-11-09 00:11:11 -0600

    Delete client directory

--- client/src/components/examples/EmptyState.tsx
+++
@@ -1,9 +0,0 @@
-import EmptyState from '../EmptyState';
-
-export default function EmptyStateExample() {
-  return (
-    <div className="h-96 bg-background">
-      <EmptyState />
-    </div>
-  );
-}
--- client/src/components/examples/TypingIndicator.tsx
+++
@@ -1,9 +0,0 @@
-import TypingIndicator from '../TypingIndicator';
-
-export default function TypingIndicatorExample() {
-  return (
-    <div className="p-6 bg-background">
-      <TypingIndicator />
-    </div>
-  );
-}
